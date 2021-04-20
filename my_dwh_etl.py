2fcf3cd44ff9eae7bc1b13be057daa0117ccb7e0
forgeekbrains2020 <72750543+forgeekbrains2020@users.noreply.github.com>
2021-04-20 09:31:24 +0700

    Update my_dwh_etl.py

--- my_dwh_etl.py
+++ my_dwh_etl.py
@@ -13,7 +13,7 @@ default_args = {
 }
 
 dag = DAG(
-    USERNAME + '_dwh_etl',
+    USERNAME + 'my_dwh_etl',
     default_args=default_args,
     description='DWH ETL tasks',
     schedule_interval="0 0 1 1 *",
