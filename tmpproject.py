5f4c944f2fb0e66fe8c04c41182c6e7d85917eeb
forgeekbrains2020 <72750543+forgeekbrains2020@users.noreply.github.com>
2021-05-17 22:33:40 +0700

    Update tmpproject.py

--- tmpproject.py
+++ tmpproject.py
@@ -13,7 +13,7 @@ default_args = {
 }
 
 dag = DAG(
-    USERNAME + '_tmpproject_etl',
+    USERNAME + '_tmp1project_etl',
     default_args=default_args,
     description='DWH ETL test tasks',
     schedule_interval="@yearly",
