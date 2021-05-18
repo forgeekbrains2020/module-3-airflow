cea43da4ceddc2f9bf7a2d0dd4d8b8e22acade52
forgeekbrains2020 <72750543+forgeekbrains2020@users.noreply.github.com>
2021-05-18 13:12:37 +0700

    Update tmpproject.py

--- tmpproject.py
+++ tmpproject.py
@@ -344,9 +344,76 @@ SELECT * FROM records_to_insert
     """
 )
 
-all_links_loaded >> [dds_sat_user_details, dds_sat_payment]
+
+
+dds_sat_traffic_user_device = PostgresOperator(
+    task_id="dds_sat_traffic_user_device",
+    dag=dag,
+    # postgres_conn_id="postgres_default",
+    sql="""
+INSERT into ayashin.pj_dds_sat_traffic_user_device (traffic_pk, traffic_hashdiff, bytes_sent, bytes_received, effective_from, load_date, record_source)
+WITH source_data AS (
+    SELECT a.traffic_pk,
+           a.traffic_hashdiff,
+           a.bytes_sent,
+           a.bytes_received,
+           a.effective_from,
+           a.load_date,
+           a.record_source
+    --FROM ayashin.pj_ods_v_payment a where a.load_date =  '2014-01-01 00:00:00.000000 +00:00'
+    FROM ayashin.pj_ods_v_traffic a where extract (year from a.timestamp) =  '2013'
+),
+update_records AS (
+         SELECT a.traffic_pk,
+                a.traffic_hashdiff,
+                a.bytes_sent,
+                a.bytes_received,
+                a.effective_from,
+                a.load_date,
+                a.record_source
+         FROM ayashin.pj_dds_sat_traffic_user_device a
+                  JOIN source_data as b
+                      ON a.traffic_pk = b.traffic_pk
+    where a.load_date <= (select max(load_date) from source_data)
+     ),
+     latest_records AS (
+         SELECT *
+         FROM (SELECT c.traffic_pk,
+                      c.traffic_hashdiff,
+                      c.bytes_sent,
+                      c.bytes_received,
+                      c.effective_from,
+                      c.load_date,
+                      c.record_source
+                      ,
+                      CASE
+                          WHEN (rank() OVER (PARTITION BY c.traffic_pk ORDER BY c.load_date DESC)) = 1 THEN 'Y'::text
+                          ELSE 'N'::text
+                          END AS latest
+               FROM update_records c) s
+         WHERE s.latest = 'Y'::text
+     ),
+     records_to_insert AS (
+         SELECT e.traffic_pk,
+                e.traffic_hashdiff,
+                e.bytes_sent,
+                e.bytes_received,
+                e.effective_from,
+                e.load_date,
+                e.record_source
+         FROM source_data e
+                  LEFT JOIN latest_records ON latest_records.traffic_hashdiff = e.traffic_hashdiff and latest_records.traffic_pk = e.traffic_pk
+         WHERE latest_records.traffic_hashdiff IS NULL
+     )
+SELECT * FROM records_to_insert
+    """
+)
+
+
+
+all_links_loaded >> [dds_sat_user_details, dds_sat_payment, dds_sat_traffic_user_device]
 all_sats_loaded = DummyOperator(task_id="all_sats_loaded", dag=dag)
-[dds_sat_user_details, dds_sat_payment] >> all_sats_loaded
+[dds_sat_user_details, dds_sat_payment, dds_sat_traffic_user_device] >> all_sats_loaded
 
 '''ods_clear = PostgresOperator(
     task_id="ods_clear",
